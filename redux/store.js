92596ba0f44179be8bc3f221ed3bc13c7652cf2f
rdev.byte <rdev.byte@gmail.com>
2023-10-08 20:23:27 -0600

    Attempted to debug store.js

--- redux/store.js
+++ redux/store.js
@@ -1,3 +1,4 @@
+/*
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -27,3 +28,36 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
+*/
+
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import userReducer from '../Slice/userSlice';
+// import appointmentsReducer from './path/to/appointmentsSlice'; // Update with your actual path
+// import notificationsReducer from './path/to/notificationsSlice'; // Update with your actual path
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  debug: true
+};
+
+const rootReducer = persistReducer(
+  persistConfig,
+  {
+    user: userReducer, // Use the userReducer here
+    appointments: appointmentsReducer,
+    notifications: notificationsReducer,
+  }
+);
+
+export const store = configureStore({
+  reducer: rootReducer, // Pass the rootReducer here
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+
+export const persistor = persistStore(store);
+
+export default appointmentsReducer;
