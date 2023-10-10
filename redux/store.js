3258e619e1f21046fc29565f4b7183c9ae7bdfd1
rdev.byte <rdev.byte@gmail.com>
2023-10-09 22:14:00 -0600

    continued to debug

--- redux/store.js
+++ redux/store.js
@@ -1,26 +1,17 @@
-/*
+import { combineReducers } from 'redux';
+import userReducer from '../Slice/userSlice';
+import appointmentsReducer from '../Slice/appointmentsSlice';
+import notificationsReducer from '../Slice/notificationsSlice';
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// import userReducer from './slices/userSlice';
-// import appointmentsReducer from './slices/appointmentsSlice';
-// import notificationsReducer from './slices/notificationsSlice';
+const rootReducer = combineReducers({
+  user: userReducer,
+  appointments: appointmentsReducer,
+  notifications: notificationsReducer,
+});
 
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  debug: true
-};
+// Rest of your store configuration...
 
-const rootReducer = persistReducer(
-  persistConfig,
-  {
-    user: userReducer,
-    appointments: appointmentsReducer,
-    notifications: notificationsReducer,
-  }
-);
 
 export const store = configureStore({
   reducer: rootReducer,
@@ -28,36 +19,6 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
-*/
 
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import userReducer from '../Slice/userSlice';
-// import appointmentsReducer from './path/to/appointmentsSlice'; // Update with your actual path
-// import notificationsReducer from './path/to/notificationsSlice'; // Update with your actual path
-
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  debug: true
-};
-
-const rootReducer = persistReducer(
-  persistConfig,
-  {
-    user: userReducer, // Use the userReducer here
-    appointments: appointmentsReducer,
-    notifications: notificationsReducer,
-  }
-);
-
-export const store = configureStore({
-  reducer: rootReducer, // Pass the rootReducer here
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-});
-
-export const persistor = persistStore(store);
-
-export default appointmentsReducer;
+// Remove this line
+// export default appointmentsReducer;
