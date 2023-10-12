76167bd3f642d6efddd561a2c8c9212f1e687491
rdev.byte <rdev.byte@gmail.com>
2023-10-11 19:58:18 -0600

    changes to store and LoadingComponent

--- redux/store.js
+++ redux/store.js
@@ -3,22 +3,27 @@ import userReducer from '../Slice/userSlice';
 import appointmentsReducer from '../Slice/appointmentsSlice';
 import notificationsReducer from '../Slice/notificationsSlice';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// Combine your reducers
 const rootReducer = combineReducers({
   user: userReducer,
   appointments: appointmentsReducer,
   notifications: notificationsReducer,
 });
 
-// Rest of your store configuration...
+// Setup persist configuration
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export const persistor = persistStore(store);
-
-// Remove this line
-// export default appointmentsReducer;
